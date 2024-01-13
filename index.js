911a8f6fae1e20039a604f224f788fdc1cef120c
Overload110 <149197288+Overload110@users.noreply.github.com>
2024-01-13 13:10:59 -0600

    Log json in console

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ document.querySelector("#weatherForm").addEventListener("submit", function(e){
 async function getWeatherData(location){
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`);
     const data = await response.json();
+    console.log(data)
     displayWeatherData(data);
   }
   
   function displayWeatherData(data) {
     const weatherInfoDiv = document.querySelector("#weatherInfo");
-    weatherInfoDiv.innerText = `It is currently ${data.main.temp} degrees and ${data.weather[0].description} in ${data.name}`;
+    weatherInfoDiv.innerText = `It is currently ${data.current.temp_f} degrees and ${data.current.condition.text} in ${data.location.name}, ${data.location.region}.`;
   }
